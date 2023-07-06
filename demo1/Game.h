30765cc7931e7330ffefadc183fd015d8a3859f8
godmin <godmin@LAPTOP-ELIRE78U>
2023-07-06 17:39:38 +0800

    添加项目文件。

---
+++ demo1/Game.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "SFML\Window.hpp"
+#include "SFML\Graphics.hpp"
+
+
+class Game
+{
+public:
+	static void Start();
+
+private:
+	static bool IsExiting();
+	static void GameLoop();
+
+	static void ShowSplashScreen();//开始制作开始背景后添加
+	static void ShowMenu();//开始制作主菜单后增加
+
+	enum GameState {
+		Uninitialized, ShowingSplash, Paused,
+		ShowingMenu, Playing, Exiting
+	};
+
+	static GameState _gameState;
+	static sf::RenderWindow _mainWindow;
+};
---
+++ demo1/pang.cpp
@@ -0,0 +1,13 @@
+// pang.cpp : Defines the entry point for the console application.
+//
+
+
+#include "stdafx.h"
+#include "Game.h"
+
+int main(int argc, sf::String argv[])
+{
+	Game::Start();
+
+	return 0;
+}
